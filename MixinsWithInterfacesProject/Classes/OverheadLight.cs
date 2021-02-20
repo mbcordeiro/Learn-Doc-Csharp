051a679808e4cf62943ad0415ad21d4b6aa4b86e
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-20 00:41:30 -0300

    Mixins with interface

---
+++ MixinsWithInterfacesProject/Classes/OverheadLight.cs
@@ -0,0 +1,17 @@
+﻿using MixinsWithInterfacesProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MixinsWithInterfacesProject.Classes
+{
+    class OverheadLight : ILight, ITimerLight, IBlinkingLight
+    {
+        private bool isOn;
+        public bool IsOn() => isOn;
+        public void SwitchOff() => isOn = false;
+        public void SwitchOn() => isOn = true;
+
+        public override string ToString() => $"The light is {(isOn ? "on" : "off")}";
+    }
+}
---
+++ MixinsWithInterfacesProject/Interfaces/ILight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MixinsWithInterfacesProject.Interfaces
+{
+    public enum PowerStatus
+    {
+        NoPower,
+        ACPower,
+        FullBattery,
+        MidBattery,
+        LowBattery
+    }
+
+    public interface ILight
+    {
+        void SwitchOn();
+        void SwitchOff();
+        bool IsOn();
+        public PowerStatus Power() => PowerStatus.NoPower;
+    }
+}
