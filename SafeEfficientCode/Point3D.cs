ce53ebe67933983e6a3281640d430f8e8e09d7a8
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-02-24 23:51:24 -0300

    ref-readonly-struct

---
+++ SafeEfficientCode/Point3D.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeEfficientCode
+{
+    public struct Point3D
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        private static Point3D origin = new Point3D();
+        public static ref readonly Point3D Origin => ref origin;
+    }
+
+    readonly public struct ReadonlyPoint3D
+    {
+        public ReadonlyPoint3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+        private static readonly ReadonlyPoint3D origin = new ReadonlyPoint3D();
+        public static ref readonly ReadonlyPoint3D Origin => ref origin;
+    }
+}
---
+++ SafeEfficientCode/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SafeEfficientCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pt1 = new Point3D { X = 3, Y = 4, Z = 5 };
+            var pt2 = new Point3D { X = 10, Y = 8, Z = 5 };
+
+            var distance = CalculateDistance(pt1, pt2);
+            var fromOrigin = CalculateDistance(pt1, new Point3D());
+
+            distance = CalculateDistance(in pt1, in pt2);
+            distance = CalculateDistance(in pt1, new Point3D());
+            distance = CalculateDistance(pt1, in Point3D.Origin);
+
+            fromOrigin = CalculateDistance2(pt1);
+
+            var originValue = Point3D.Origin;
+            ref readonly var originReference = ref Point3D.Origin;
+        }
+
+        private static double CalculateDistance(Point3D point1, Point3D point2)
+        {
+            double xDifference = point1.X - point2.X;
+            double yDifference = point1.Y - point2.Y;
+            double zDifference = point1.Z - point2.Z;
+
+            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+        }
+
+        private static double CalculateDistance(in Point3D point1, in Point3D point2)
+        {
+            double xDifference = point1.X - point2.X;
+            double yDifference = point1.Y - point2.Y;
+            double zDifference = point1.Z - point2.Z;
+
+            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+        }
+
+        private static double CalculateDistance2(in Point3D point1, in Point3D point2 = default)
+        {
+            double xDifference = point1.X - point2.X;
+            double yDifference = point1.Y - point2.Y;
+            double zDifference = point1.Z - point2.Z;
+
+            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+        }
+
+        private static double CalculateDistance3(in ReadonlyPoint3D point1, in ReadonlyPoint3D point2 = default)
+        {
+            double xDifference = point1.X - point2.X;
+            double yDifference = point1.Y - point2.Y;
+            double zDifference = point1.Z - point2.Z;
+
+            return Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+        }
+    }
+}
+}
